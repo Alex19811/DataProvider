d5aa2972ad6e93355f648b37429676779ef2c4fd
Alex19811 <aleksandrburlutskiy@gmail.com>
2019-06-03 19:35:05 +0300

    03/06/2019

--- src/test/java/com/mainacad/MainTest.java
+++ src/test/java/com/mainacad/MainTest.java
@@ -8,12 +8,18 @@ import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
-public class MainTest
-{
+
+public class MainTest {
+
+
     //Пример DataProvider метода
     @DataProvider
-    public Object[] myDataProvider(){
+    public Object[] myDataProvider() {
         return new Object[]{
                 "Data 1 from my data provider",
                 "Data 2 from my data provider",
@@ -28,10 +34,25 @@ public class MainTest
     public void testWithDataProvider(String inputData) throws InterruptedException {
         System.out.println("Test received: " + inputData);
         Thread.sleep(1000);
-        Assert.assertTrue( true );
+        Assert.assertTrue(true);
     }
 
 
+    @DataProvider
+    public Object[] testDataProvider () throws IOException {
+        BufferedReader device = new BufferedReader(new FileReader("deviceList.txt"));
+        List<String> lines = new ArrayList<>();
+        String line;
+        while ((line = device.readLine()) != null) {
+            lines.add(line);
+            // System.out.println(device);
+        }
+
+        Object[] objects = lines.toArray();
+// If you want to convert to a String[]
+
+        return objects;
+    }
 
 
     //TODO: Напишите @DataProvider метод
@@ -42,15 +63,15 @@ public class MainTest
     //TODO: Перепишите тест testGoogleSearch с использованием @DataProvider
     // Тест должен выполнится для каждого девайса из списка в deviceList.txt
 
-    @Test()
-    public void testGoogleSearch() throws InterruptedException {
+    @Test(dataProvider = "testDataProvider")
+    public void testGoogleSearch(String device) throws InterruptedException {
         //Передаём девайс для эмуляции и создаём драйвер
-        WebDriver driver = Main.getDriver("Nexus 10");
+        WebDriver driver = Main.getDriver(device);
 
         driver.get("http://www.google.com/xhtml");
         Thread.sleep(3000);
 
-        WebElement searchBox =driver.findElement(By.name("q"));
+        WebElement searchBox = driver.findElement(By.name("q"));
         searchBox.sendKeys("automation");
         searchBox.submit();
         Thread.sleep(3000);
